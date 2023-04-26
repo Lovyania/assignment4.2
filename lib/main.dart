af0bbb43648413850a13df130efbb685f4b75138
AnthraaaX <107903512+AnthraaaX@users.noreply.github.com>
2023-04-26 22:48:46 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
       home: BandsScreen(),
     );
   }
