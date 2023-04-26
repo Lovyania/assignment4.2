e665405ac79152be710a3d315a9d6d21c7cbe03c
AnthraaaX <107903512+AnthraaaX@users.noreply.github.com>
2023-04-27 01:59:45 +0800

    added member model

--- lib/models/membermodel.dart
+++ lib/models/membermodel.dart
@@ -1,1 +1,35 @@
+class Member {
+  int? memberId;
+  String member;
+  String instrument;
+  int bandId;
+  int songId;
 
+  Member({
+    this. memberId,
+    required this.member,
+    required this.instrument,
+    required this.bandId,
+    required this.songId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'memberId': memberId,
+      'member': member,
+      'instrument': instrument,
+      'bandId': bandId,
+      'songId': songId,
+    };
+  }
+
+  static Member fromMap(Map<String, dynamic> map) {
+    return Member(
+      memberId: map['memberId'],
+      member: map['member'],
+      instrument: map['instrument'],
+      bandId: map['bandId'],
+      songId: map['songId'],
+    );
+  }
+}
